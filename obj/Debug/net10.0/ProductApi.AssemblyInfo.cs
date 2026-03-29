2fa629d4f4241c6b38b88d20080a04d44e3fb8d2
fahim <fahimweb.com>
2026-03-30 02:34:48 +0600

    added some changes

--- obj/Debug/net10.0/ProductApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15405a6fd953d2d79a3b18395eefeb1347e8744")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127fb6bfbf73067c6e076b72d7dac758a49ec9f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
